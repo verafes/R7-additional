2af7cc5f38b94acf59f5faa69213f288341771de
verafes <verafes@gmail.com>
2024-08-25 17:00:26 -0700

    Fix Faker usage in customer factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 FactoryBot.define do
-  factory :customer do
+  factory :customer do |f|
     f.first_name { Faker::Name.first_name }
     f.last_name { Faker::Name.last_name }
     f.phone { Faker::Number.number(digits: 10) }

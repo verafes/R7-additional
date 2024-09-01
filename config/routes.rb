54200747cca61cc7090bf3d5f3d0b39f914aa70d
verafes <verafes@gmail.com>
2024-08-31 23:14:47 -0700

    add access to new_orders_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/edit'
-  get 'orders/create'
-  get 'orders/update'
-  get 'orders/destroy'
+  resources :orders
+
   resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
